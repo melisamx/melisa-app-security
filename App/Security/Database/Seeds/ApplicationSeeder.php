9b6e0ba95e5c0527a89913f03b5fe6787ce8a55a
Luis Josafat Heredia Contreras <luisyosafat@gmail.com>
2017-04-03 09:32:58 -0500

    update version

--- App/Security/Database/Seeds/ApplicationSeeder.php
+++ App/Security/Database/Seeds/ApplicationSeeder.php
@@ -18,7 +18,7 @@ class ApplicationSeeder extends InstallSeeder
             'description'=>'Application Security',
             'nameSpace'=>'Melisa.security',
             'typeSecurity'=>'art',
-            'version'=>'1.0.5'
+            'version'=>'1.1.0'
         ]);
         
     }
